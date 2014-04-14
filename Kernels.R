76544b03ee605fad0b692e445bd9ccd5033ca318
cdellaie <dellaiera.clement@gmail.com>
2014-04-14 19:13:05 +0200

    Kernels
    Ajout de la procédure par cross validation
    Code à revoir : la courbe ROC est bien trop belle , il y a un problème.

--- Kernels.R
+++ Kernels.R
@@ -1,4 +1,5 @@
 library(kernlab)
+library(ROCR)
 
 #téléchargement des données
 
@@ -6,8 +7,9 @@ xtrain = read.table("C:/Users/Clément/Desktop/ENSAE 3A/MLCB/xtrain.txt")
 ytrain = read.table("C:/Users/Clément/Desktop/ENSAE 3A/MLCB/ytrain.txt")
 test = read.table("C:/Users/Clément/Desktop/ENSAE 3A/MLCB/xtest.txt")
 
-FinalTest=t(matrix(test))
-
+matrix(test)
+ttest=as.matrix(test[2:93])
+FinalTest=t(ttest)
 
 matrix(xtrain)
 data=as.matrix(xtrain[,2:185])
@@ -20,29 +22,127 @@ ytrain2=ytrain[1:150,]
 xtest=datat[151:184,]
 ytest=ytrain[151:184,]
 
+##################################################################################################################
 #essai sur les set d'entraînements avec plusieurs noyaux
-#lemeilleur sera choisi pour être entrainé sur la base complète et prévoir sur les données à traiter
+#le meilleur sera choisi pour être entrainé sur la base complète et prévoir sur les données à traiter
 #tanhdot,laplacedot,besseldot,anovadot
-
-GaussianKernel <- ksvm(xtrain2,ytrain2,type="C-svc",kernel="rbfdot",kpar=list(sigma=1),scale=FALSE)
-predict(PolyKernel,xtest)-ytest
-plot(PolyKernel,data=xtest)
-
-PolyKernel = ksvm(xtrain2,ytrain2,type="C-svc",kernel=polydot(degree=2))
-predict(PolyKernel,xtest)-ytest
-plot(PolyKernel,data=xtest)
+##################################################################################################################
 
 LinearKernel <- ksvm(xtrain2,ytrain2,type="C-svc",kernel="vanilladot")
-predict(svp,xtest)-ytest
-plot(svp,data=xtest)
+plot(LinearKernel,data=xtest)
+ypred=predict(LinearKernel,xtest)
+table(ytest,ypred)
+sum(ypred==ytest)/length(ytest)
+
+GaussianKernel <- ksvm(xtrain2,ytrain2,type="C-svc",kernel="rbfdot",kpar=list(sigma=1),scale=FALSE)
+plot(GaussianKernel,data=xtest)
+ypred=predict(GaussianKernel,xtest)
+table(ytest,ypred)
+sum(ypred==ytest)/length(ytest)
+
+PolyKernel = ksvm(xtrain2,ytrain2,type="C-svc",kernel=polydot(degree=2))
+plot(PolyKernel,data=xtest)
+ypred=predict(PolyKernel,xtest)
+table(ytest,ypred)
+sum(ypred==ytest)/length(ytest)
 
 AnovaKernel = ksvm(xtrain2,ytrain2,type="C-svc",kernel="anovadot")
-predict(PolyKernel,xtest)-ytest
-plot(PolyKernel,data=xtest)
+plot(AnovaKernel,data=xtest)
+ypred=predict(AnovaKernel,xtest)
+table(ytest,ypred)
+sum(ypred==ytest)/length(ytest)
 
-#Anova et Poly donnent les meilleurs résultats
+TanhKernel = ksvm(xtrain2,ytrain2,type="C-svc",kernel="tanhdot")
+plot(TanhKernel,data=xtest)
+ypred=predict(TanhKernel,xtest)
+table(ytest,ypred)
+sum(ypred==ytest)/length(ytest)
 
-BestKernel = ksvm(datat,ytrain,type="C-svc",kernel=polydot(degree=2))
+BesselKernel = ksvm(xtrain2,ytrain2,type="C-svc",kernel="besseldot")
+plot(BesselKernel,data=xtest)
+ypred=predict(BesselKernel,xtest)
+table(ytest,ypred)
+sum(ypred==ytest)/length(ytest)
+
+LaplaceKernel = ksvm(xtrain2,ytrain2,type="C-svc",kernel="laplacedot")
+plot(LaplaceKernel,data=xtest)
+ypred=predict(LaplaceKernel,xtest)
+table(ytest,ypred)
+sum(ypred==ytest)/length(ytest)
+
+#############################################################################
+#Linear, Anova et Poly donnent les meilleurs résultats : 0.67 accuracy ######
+#############################################################################
+
+BestKernel = ksvm(datat,ytrain,type="C-svc",kernel=polydot(degree=2),scale=1)
 predict(BestKernel,FinalTest)
+
+##################################################################################################################
+##################################################################################################################
+###                                         Cross Validation                                                   ###
+##################################################################################################################
+##################################################################################################################
+
+#############################
+## Fonctions préliminaires ##
+#############################
+
+cv.folds <- function(n,nfolds=3)
+## Randomly split the n samples into folds
+## Returns a list of nfolds lists of indices, each corresponding to a fold
+{
+ 	return(split(sample(n),rep(1:nfolds,length=n)))
+}
+
+cvpred.ksvm <- function(x,y,folds=3,predtype="response",...)
+## Return a vector of predictions by cross-validation
+## 'predtype' should be one of response (by default), decision or probabilities, depending the prediction we want (SVM label, score or probability, see predict.ksvm())
+## Additional parameters are passed to ksvm() to train the SVM
+{
+ 	n <- length(y)
+ 	ypred <- numeric(n)
+ 	s <- cv.folds(n,folds)
+ 	for (i in seq(folds)) {
+ 		m <- ksvm(x[-s[[i]],],y[-s[[i]]],...)
+ 		ypred[s[[i]]] <- predict(m,x[s[[i]],],type=predtype)
+ 		}
+ 	invisible(ypred)
+ }
+
+#######################
+#######################
+#######################
+
+PolyKernel = ksvm(xtrain2,ytrain2,type="C-svc",kernel=polydot(degree=2))
+ypred=predict(PolyKernel,xtest)
+table(ytest,ypred)
+sum(ypred==ytest)/length(ytest)
+
+ypredscore = predict(PolyKernel,xtest,type="decision")
+table(ypredscore > 0,ypred)
+
+pred <- prediction(ypredscore,ytest)
+perf <- performance(pred, measure = "prec", x.measure = "rec") 
+plot(perf)
+
+# We compute the prediction vector by cross-validation
+
+k=5	
+ypredscorecv <- cvpred.ksvm(x,y,folds=k,type="C-svc",kernel=polydot(degree=2),C=1,scaled=c(),predtype="decision")
+
+# Check the performance
+print(table(ypredscorecv > 0,y))
+pred <- prediction(ypredscorecv,y)
+
+#Courbe ROC
+perf <- performance(pred, measure = "tpr", x.measure = "fpr") 
+plot(perf)
+
+# Estimate the CV error with ksvm directly, and compare
+svp <- ksvm(x,y,type="C-svc",kernel='vanilladot',C=1,scaled=c(),cross=5)
+print(cross(svp))
+print(1-sum((ypredscorecv>0)==(y==1))/n)
+
+
 
  
